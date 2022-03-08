a88caeabae2a0b55052313cd212716e9cbd47bc0
matheus monteiro <matheusyanmonteiro@gmail.com>
2022-03-08 09:38:14 -0300

    create the first steps for documentation. Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
+//instance of express
 const app = express();
-
+//express
 app.use(express.json());
+//swagger init documentation
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerFile));
+//routes of application
 app.use(router);
+//localhost listen 
 app.listen(3333, () => console.log("Server is running!"));
