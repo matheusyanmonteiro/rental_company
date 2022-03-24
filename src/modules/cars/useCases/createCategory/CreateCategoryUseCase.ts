f4e362da3ab9cc131fe7c1082d66aadb4feeb778
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-03-24 17:47:05 -0300

    create tsyringe injection

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from 'tsyringe';
+
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest
@@ -6,9 +8,13 @@ interface IRequest
   description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase 
 {
-  constructor(private categoriesRepository: ICategoriesRepository){}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+    ) {}
 
   async execute({ description, name }: IRequest): Promise<void>
   {
