87a61acd5190a40223763320533fe4fb0db6e4d3
matheus <matheusyanmonteiro@gmail.com>
2022-05-24 00:04:27 -0300

    create integration test

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,43 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import { router } from './routes';
+import swaggerFile from '../../../swagger.json';
+
+//database
+//import "@shared/infra/typeorm";
+import createConnection from "@shared/infra/typeorm"
+//dependency injection
+import "@shared/container";
+import { AppError } from "@shared/errors/AppError";
+
+
+
+createConnection();
+//instance of express
+const app = express();
+//express
+app.use(express.json());
+//swagger init documentation
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerFile));
+//routes of application
+app.use(router);
+
+// middle of error
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+  })
+})
+
+export { app };
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -6,7 +6,10 @@ export default async(host = "localhost"): Promise<Connection> => {
 
    return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: process.env.NODE_ENC === "test"? "localhost" : host,
+      database: process.env.NODE_ENV === "test"
+        ? "rentex_test"
+        : defaultOptions.database,
      })
   );
 }
