12e40f24d2f78024e22a39d897ae5d799d6670d4
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-08-19 17:21:48 -0300

    create route for reset passowrd

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,10 +1,13 @@
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/resetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPassword/SendForgotPasswordMailController";
 import { Router } from "express";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailCOntroller = new SendForgotPasswordMailController();
+const resetPasswordController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailCOntroller.handle);
+passwordRoutes.post("/reset", resetPasswordController.handle);
 
 export { passwordRoutes };
